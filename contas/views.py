8208469195191939f3ca98b16ff83ad1639f02b3
Marcos Vinicius <marcosvinicius.mecatronica@gmail.com>
2020-03-08 22:43:42 -0400

    Add files via upload

---
+++ contas/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
+# Create your views here.
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/index.html', data)
+
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+    if(form.is_valid()):
+        form.save()
+        return redirect('url_listagem')
+    return render(request, 'contas/form.html', {'form': form})
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
